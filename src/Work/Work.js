653d274c65772060a961d1ad6e308d978bdfe9ab
rong12345 <1165895564@qq.com>
2016-09-22 16:28:53 +0800

    work

--- src/Work/Work.js
+++ src/Work/Work.js
@@ -3,8 +3,49 @@ import React from 'react'
 class Work extends React.Component {
   render () {
     return(
-      <div>
-        Work
+      <div className="work-container">
+        <div className="work-card">
+          <img src="http://img2.3lian.com/2014/f5/44/d/43.jpg" />
+          <div className="work-text">
+            <h3>作品一</h3>
+            <p>2016.09.22</p>
+          </div>
+        </div>
+        <div className="work-card">
+          <img src="http://img15.3lian.com/2015/a1/27/d/202.jpg" />
+          <div className="work-text">
+            <h3>作品一</h3>
+            <p>2016.09.22</p>
+          </div>
+        </div>
+        <div className="work-card">
+          <img src="http://img15.3lian.com/2015/a1/27/d/228.jpg" />
+          <div className="work-text">
+            <h3>作品一</h3>
+            <p>2016.09.22</p>
+          </div>
+        </div>
+        <div className="work-card">
+          <img src="http://imga1.pic21.com/bizhi/131118/03993/s08.jpg" />
+          <div className="work-text">
+            <h3>作品一</h3>
+            <p>2016.09.22</p>
+          </div>
+        </div>
+        <div className="work-card">
+          <img src="http://bbs.21mould.net/data/attachment/forum/201410/25/120313ubm866elt7neezea.jpg" />
+          <div className="work-text">
+            <h3>作品一</h3>
+            <p>2016.09.22</p>
+          </div>
+        </div>
+        <div className="work-card">
+          <img src="http://imga1.pic21.com/bizhi/131217/05349/s03.jpg" />
+          <div className="work-text">
+            <h3>作品一</h3>
+            <p>2016.09.22</p>
+          </div>
+        </div>
       </div>
     )
   }
