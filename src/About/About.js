c5e595d8c6da6fb70516ae0dd15751c0b8cf7ce9
rong12345 <1165895564@qq.com>
2016-11-08 15:32:02 +0800

    finish

--- src/About/About.js
+++ src/About/About.js
@@ -1,1 +1,29 @@
 
+import React, { PropTypes } from 'react'
+import { Link } from 'react-router';
+
+class About extends React.Component {
+  render () {
+   let   styles={
+      btn:{
+        width:'80px',
+        height:'30px',
+        borderRadius:'15px',
+        backgroundColor:'#FF9E80',
+        color:"teal",
+        margin:'10px 0',
+        textAlign:'center',
+        lineHight:'30px'
+      }
+    }
+    return(
+      <div className="about-container">
+        <h3></h3>
+
+
+      </div>
+    )
+  }
+}
+
+export default About;
--- src/Work/Work.js
+++ src/Work/Work.js
@@ -1,8 +1,10 @@
 import React, { PropTypes } from 'react'
-import Img1 from '../img/1.jpg';
-import Img2 from '../img/2.jpeg';
-import Img3 from '../img/3.jpeg';
-
+import Img1 from '../images/1.jpg';
+import Img2 from '../images/2.jpg';
+import Img3 from '../images/3.jpg';
+import Img4 from '../images/4.jpeg';
+import Img5 from '../images/5.jpeg';
+import Img6 from '../images/6.jpg';
 
 class Work extends React.Component {
   render () {
@@ -11,7 +13,7 @@ class Work extends React.Component {
 
 
         <div className="work-card">
-              <a href="https://xixilide.github.io/"><img src={Img1} /></a>
+              <a href="https://rong12345.github.io/"><img src={Img1} /></a>
                   <div className="work-text">
                     <h3>作品一</h3>
                     <p>2016.5.6</p>
@@ -19,7 +21,7 @@ class Work extends React.Component {
                 </div>
 
       <div className="work-card">
-            <a href="http://model.100autoshow.com/"><img src="http://img4.duitang.com/uploads/item/201510/18/20151018120957_W3AGU.jpeg" /></a>
+            <a href="http://www.baidu.com/"><img src={Img2} /></a>
                 <div className="work-text">
                   <h3>作品二</h3>
                   <p>2015.10.6</p>
@@ -27,7 +29,7 @@ class Work extends React.Component {
               </div>
 
     <div className="work-card">
-          <a href="http://www.cnpickups.com"><img src={Img2} /></a>
+          <a href="http://www.baidu.com/"><img src={Img3} /></a>
               <div className="work-text">
                 <h3>作品三</h3>
                 <p>2016.1.5</p>
@@ -35,9 +37,23 @@ class Work extends React.Component {
     </div>
 
   <div className="work-card">
-        <a href="http://www.cnpickups.com"><img src="http://img5.duitang.com/uploads/item/201507/15/20150715012629_LQ4T5.jpeg" /></a>
+        <a href="http://www.baidu.com/"><img src={Img4} /></a>
           <div className="work-text">
               <h3>作品四</h3>
+              <p>2016.4.6</p>
+        </div>
+  </div>
+  <div className="work-card">
+        <a href="http://www.baidu.com/"><img src={Img5} /></a>
+          <div className="work-text">
+              <h3>作品五</h3>
+              <p>2016.4.6</p>
+        </div>
+  </div>
+  <div className="work-card">
+        <a href="http://www.baidu.com/"><img src={Img6} /></a>
+          <div className="work-text">
+              <h3>作品六</h3>
               <p>2016.4.6</p>
         </div>
   </div>
